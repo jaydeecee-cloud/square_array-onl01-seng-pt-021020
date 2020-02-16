8a326c5f3e9be92eed9b4c4fab0e830910f53ee0
Joseph Choi <jdc07@live.com>
2020-02-16 00:55:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,4 @@
 def square_array(array)
-  newarray = []
-  array.each do |element|
-  newarray << element * element
-  end
-  return newarray
-end
-
-def square_array(array)
   array.collect do |element|
   element * element
   end
